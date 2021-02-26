b706b4cc7e074e2a71310379670929cdfb4cf05a
Maxim Egorov <13maximegorov@gmail.com>
2021-02-26 23:59:50 +0500

    config libs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
+
+import {MaterialModule} from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { HomePageComponent } from './home-page/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import { dia, ui, shapes } from '@clientio/rappid';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas') canvas: ElementRef;
+
+  private graph: dia.Graph;
+  private paper: dia.Paper;
+  private scroller: ui.PaperScroller;
 
   constructor() { }
 
   ngOnInit(): void {
+    const graph = this.graph = new dia.Graph({}, { cellNamespace: shapes });
+
+    const paper = this.paper = new dia.Paper({
+      model: graph,
+      background: {
+        color: '#F8F9FA',
+      },
+      frozen: true,
+      async: true,
+      cellViewNamespace: shapes
+    });
+
+    const scroller = this.scroller = new ui.PaperScroller({
+      paper,
+      autoResizePaper: true,
+      cursor: 'grab'
+    });
+
+    scroller.render();
+
+    const rect = new shapes.standard.Rectangle({
+      position: { x: 100, y: 100 },
+      size: { width: 100, height: 50 },
+      attrs: {
+        label: {
+          text: 'Hello World'
+        }
+      }
+    });
+
+    this.graph.addCell(rect);
+  }
+
+  ngAfterViewInit(): void {
+    const { scroller, paper, canvas } = this;
+    canvas.nativeElement.appendChild(this.scroller.el);
+    scroller.center();
+    paper.unfreeze();
   }
 
 }
